a34cb5c834ebcdebad110d284ca874095fbd4fb9
Hindole Dutta <hindole@Hindoles-Laptop.local>
2025-03-30 17:56:39 +0530

    Switch to run_console() for headless Render auth

--- gmail_auth.py
+++ gmail_auth.py
@@ -19,7 +19,7 @@ def authenticate_gmail():
     if not creds or not creds.valid:
         flow = InstalledAppFlow.from_client_secrets_file(
             'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=8000)
+        creds = flow.run_console()
         # Save credentials for future use
         with open('token.pkl', 'wb') as token:
             pickle.dump(creds, token)
